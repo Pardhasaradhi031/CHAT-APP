29ea731cb92c8cb643e8a0e81025f899ee40eec4
Pardhasaradhi031 <jai.91630@default.com>
2025-06-05 17:44:50 +0530

    add user model and create gitignore file

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,16 @@
 import express from 'express';
+import dotenv from 'dotenv';
 
+import { connectDB } from './lib/db.js';
+import authRoutes from './routes/auth.routes.js';
+
+dotenv.config();
 const app = express();
 
-app.listen(50001, () => {
-  console.log('Server is running on port 50001');
+const PORT = process.env.PORT || 5001;
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log('Server is running on port ' + PORT);
+  connectDB();
 });
