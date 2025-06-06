55b3360e14f677453a02274d131797d7375c34f9
Pardhasaradhi031 <jai.91630@default.com>
2025-06-06 12:22:35 +0530

    Create routes for updating profiles, checking auth

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,13 +1,18 @@
 import express from 'express';
 
-import { signup, login, logout } from '../controllers/auth.controller.js';
+import { signup, login, logout, checkAuth } from '../controllers/auth.controller.js';
+import { updateProfile } from '../controllers/auth.controller.js';
+import protectRoute from "../middlewares/auth.middleware.js";
+
 
 const router = express.Router();
 
 router.post('/signup', signup);
-
 router.post('/login', login);
-
 router.post('/logout', logout);
+
+router.put('/update-profile', protectRoute, updateProfile);
+
+router.get('/checkAuth', protectRoute, checkAuth);
 
 export default router;
